76123fc315c7d043a6765d608c34b4fdd896ef83
Alessandro-Villani <alessvill@libero.it>
2023-01-09 15:50:07 +0100

    added datas from vue - end

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 console.log('VUE OK', Vue);
+
+const app = Vue.createApp({
+    data(){
+        return{
+            firstName: 'Alessandro',
+            lastName: 'Villani',
+            picture: 'https://picsum.photos/200',
+            description: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim nostrum distinctio quod possimus doloremque neque vel. Totam dolore vel, illum, dolorum corrupti iure consectetur asperiores quas iste natus, cum saepe.'
+        }
+    }
+});
+
+app.mount('#root');
